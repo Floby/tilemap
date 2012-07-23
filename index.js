ba35c09142e99a08dc3b41e6c765f76c21998160
James Halliday <mail@substack.net>
2012-07-23 02:31:52 -0700

    compute offsets dynamically

--- index.js
+++ index.js
@@ -7,37 +7,38 @@ module.exports = function (width, height) {
 function TileMap (width, height) {
     this.element = document.createElement('div');
     this.paper = raphael(this.element, width, height);
+    this.size = [ width, height ];
+    this.center = [ 0, 0 ];
 }
 
 TileMap.prototype.resize = function (width, height) {
+    this.size = [ width, height ];
     this.paper.setSize(width, height);
 }
 
-TileMap.prototype.createTile = function (tx, ty) {
-    var pt = fromTile(tx, ty);
-    var x = pt[0], y = pt[1];
-    
+TileMap.prototype.createTile = function (x, y) {
+    var self = this;
     var points = [
-        [ x - 0.5, y ],
-        [ x, y - 0.5 ],
-        [ x + 0.5, y ],
-        [ x, y + 0.5 ],
-    ];
-    var tile = this.paper.path(polygon(points.map(toWorld)));
+        [ x - 0.5, y + 0.5 ],
+        [ x - 0.5, y - 0.5 ],
+        [ x + 0.5, y - 0.5 ],
+        [ x + 0.5, y + 0.5 ]
+    ].map(function (pt) { return self.toWorld(pt) });
+    
+    var tile = this.paper.path(polygon(points));
     tile.attr('stroke-width', '1');
     tile.attr('fill', 'rgba(0,0,127,0.5)');
     tile.attr('stroke', 'rgba(0,0,64,0.5)');
     return tile;
 }
 
-function fromTile (x, y) {
-    return [ x / 2 + y / 2, -x / 2 + y / 2 ];
-}
-
-function toWorld (pt) {
-    var x = pt[0] + 6;
-    var y = pt[1] + 6;
-    return [ x * 100, y * 50 ];
+TileMap.prototype.toWorld = function (pt) {
+    var x = pt[0] / 2 + pt[1] / 2;
+    var y = -pt[0] / 2 + pt[1] / 2;
+    
+    var tx = x + (this.size[0] / 100 / 2) + this.center[0];
+    var ty = y + (this.size[1] / 50 / 2) + this.center[1];
+    return [ tx * 100, ty * 50 ];
 }
 
 function polygon (points) {
