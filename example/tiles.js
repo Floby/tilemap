2a6fd5b99ea22d643100eb19f6fbbaaabd28aea5
James Halliday <mail@substack.net>
2012-07-24 02:10:29 -0700

    update old example for tile.element

--- example/tiles.js
+++ example/tiles.js
@@ -5,9 +5,9 @@ grid.appendTo(document.body);
 for (var x = -10; x < 10; x++) {
     for (var y = -10; y < 10; y++) {
         var tile = grid.createTile(x, y);
-        tile.attr('fill', 'rgba(210,210,210,1.0)');
-        tile.attr('stroke-width', '1');
-        tile.attr('stroke', 'rgb(255,255,200)');
+        tile.element.attr('fill', 'rgba(210,210,210,1.0)');
+        tile.element.attr('stroke-width', '1');
+        tile.element.attr('stroke', 'rgb(255,255,200)');
     }
 }
 
@@ -15,22 +15,22 @@ window.addEventListener('resize', function (ev) {
     grid.resize(window.outerWidth, window.outerHeight);
 });
 
-grid.on('mouseover', function (tile, x, y) {
-    tile.toFront();
-    tile.attr('fill', 'rgba(255,127,127,0.8)');
+grid.on('mouseover', function (tile) {
+    tile.element.toFront();
+    tile.element.attr('fill', 'rgba(255,127,127,0.8)');
 });
 
 grid.on('mouseout', function (tile) {
-    tile.toBack();
-    tile.attr('fill', 'rgba(210,210,210,1.0)');
+    tile.element.toBack();
+    tile.element.attr('fill', 'rgba(210,210,210,1.0)');
 });
 
-grid.on('mousedown', function (tile, x, y) {
-    if (grid.itemAt(x, y)) {
-        grid.removeItem(x, y);
+grid.on('mousedown', function (tile) {
+    if (grid.itemAt(tile.x, tile.y)) {
+        grid.removeItem(tile.x, tile.y);
     }
     else grid.createItem(
         'http://substack.net/projects/datacenter/rack_0.png',
-        x, y
+        tile.x, tile.y
     );
 });
