b77e925ec9c174bbcccb49b3aad76ca15ef7d5c3
James Halliday <mail@substack.net>
2012-08-09 02:58:33 -0700

    walls work

--- example/main.js
+++ example/main.js
@@ -21,14 +21,23 @@ grid.on('keydown', function (ev) {
             tile : 'point',
             point : 'tile'
         }[mode];
+        
+        if (selectedPoint) {
+            var s = selectedPoint;
+            selectedPoint = null;
+            s.emit('mouseout');
+        }
     }
 });
 
+var selectedPoint = null;
 function onCreatePoint (pt) {
     pt.on('mouseover', function () {
         if (mode !== 'point') return;
         
-        if (!pt.active) {
+        if (!selectedPoint
+        || selectedPoint.x === pt.x
+        || selectedPoint.y === pt.y) {
             pt.element.toFront();
             pt.element.attr('fill', 'rgba(255,0,0,1)');
         }
@@ -37,22 +46,48 @@ function onCreatePoint (pt) {
     pt.on('mouseout', function () {
         if (mode !== 'point') return;
         
-        if (!pt.active) {
+        if (pt !== selectedPoint) {
             pt.element.attr('fill', 'transparent');
         }
     });
     
     pt.on('mousedown', function () {
         if (mode !== 'point') return;
+        if (!selectedPoint) {
+            pt.element.attr('fill', 'rgb(0,255,255,1)');
+            selectedPoint = pt;
+            return;
+        }
         
-        if (pt.active) {
-            pt.element.attr('fill', 'transparent');
-            pt.active = false;
+        if (selectedPoint.x !== pt.x && selectedPoint.y !== pt.y) {
+            selectedPoint.element.attr('fill', 'transparent');
+            selectedPoint = null;
+            return;
+        }
+        
+        var walls = {
+            left : 'http://substack.net/projects/datacenter/wall_left.png',
+            right : 'http://substack.net/projects/datacenter/wall_right.png'
+        };
+        
+        if (selectedPoint.y === pt.y) {
+            var x0 = Math.min(selectedPoint.x, pt.x);
+            var xt = Math.max(selectedPoint.x, pt.x);
+            for (var x = x0; x < xt; x++) {
+                grid.createItem(walls.right, x + 0.75, pt.y - 0.25);
+            }
         }
         else {
-            pt.element.attr('fill', 'rgb(0,255,255,1)');
-            pt.active = true;
+            var y0 = Math.min(selectedPoint.y, pt.y);
+            var yt = Math.max(selectedPoint.y, pt.y);
+            for (var y = y0; y < yt; y++) {
+                grid.createItem(walls.left, pt.x + 0.25, y + 0.25);
+            }
         }
+        
+        selectedPoint.element.attr('fill', 'transparent');
+        pt.element.attr('fill', 'transparent');
+        selectedPoint = null;
     });
 }
 
